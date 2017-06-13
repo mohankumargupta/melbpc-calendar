ef6967d90d9003ecf979d1f0c8cc50f81d637f20
mohankumargupta <mohangupta@live.com>
2017-06-13 13:58:51 +1000

    putting it all together now

--- src/CalendarBits/MelbPCCalendar.php
+++ src/CalendarBits/MelbPCCalendar.php
@@ -2,6 +2,9 @@
 namespace CalendarBits;
 
 require_once '/vendor/autoload.php';
+use Dompdf\Dompdf;
+
+
 
 //use Twig\Twig_Loader_Filesystem;
 
@@ -16,6 +19,19 @@ class MelbPCCalendar {
 		
 	}
 	
+	function renderCalendar($html) {
+		date_default_timezone_set('Australia/Melbourne');
+
+		$dompdf = new Dompdf();
+		$dompdf->set_option('chroot', '/calendarmelbpc');
+		$dompdf->set_option('isRemoteEnabled', true);
+		$dompdf->loadHtml($html);
+		$dompdf->set_paper('A4', 'portrait');
+        $dompdf->set_option('isHtml5ParserEnabled', TRUE);
+		$dompdf->render();
+		$dompdf->stream('document.pdf', array("Attachment" => false));		
+	}
+	
 	function render() {
 		$loader = new \Twig_Loader_Filesystem('/');
 		$twig = new \Twig_Environment($loader);
@@ -25,24 +41,23 @@ class MelbPCCalendar {
 		$boo[0]->content = array(
 		                         ["SIG meeting at MelbPC", "header1"],
                        		     ["PC HQ, Moorabbin.","header1"],
-		                         ["Non-Moorabbin meeting.", "header2"],
-		                         ["Cancelled Meeting", "header3"],
-		                         ["Changed Meeting","header4"],
-		                         ["To be confirmed","header5"]
+		                         ["Non-Moorabbin meeting.", "header1"],
+		                         ["Cancelled Meeting", "header1"],
+		                         ["Changed Meeting","header1"],
+		                         ["To be confirmed","header1"]
 								);
 								
 				
         $boo[1] = new \stdClass();
 		$boo[1]->heading = "Changes";
 		$boo[1]->content = array(
-                           ["Please advise", "header2"],
-                           ["changes to", "header2"],
-                           ["<a href=\"mailto:calendar-admin@melbpc.org.au\">calendar-admin@melbpc.org.au</a>", "header3"],						   
+                           ["Please advise", "header1"],
+                           ["changes to", "header1"]					   
 						   );
 				
 		$firstofmonth =  new \DateTime("first day of {$this->month} {$this->year}");
         $firstofmonth = $firstofmonth->format('N');
-		echo $firstofmonth;
+		//echo $firstofmonth;
 		$monthindigit = new \DateTime("{$this->month} {$this->year}");
 		$monthindigit = $monthindigit->format('n');
         $numberofdaysinmonth = cal_days_in_month(CAL_GREGORIAN, $monthindigit, $this->year);
@@ -58,8 +73,9 @@ class MelbPCCalendar {
 			
 		}
 		
-		//print_r($boo);
-		echo $twig->render('/calendar.html',array('calendardata'=>$boo));
+		//echo $twig->render('/calendar.html',array('calendardata'=>$boo));
+		$html = $twig->render('/calendar.html',array('calendardata'=>$boo));
+	    $this->renderCalendar($html);
 	}
 }
 
